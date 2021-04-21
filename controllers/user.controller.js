ef755088038a7d269e46205cc2d87d4339148429
Hasankaca1 <8039636@gmail.com>
2021-04-21 21:47:50 +0300

    adding role as aggregate for findOne

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -116,6 +116,7 @@ exports.findOneUser = (req, res) => {
   const id = req.params.id;
 
   User.findById(id)
+  .popuate("roles")
     .then((data) => {
       if (!data)
         res.status(404).send({ message: "Not found User with id " + id });
